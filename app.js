9749dec2fbfddc0af5a87f41054413dd58e2e2cf
soniv ahmad <sonivahmad@gmail.com>
2023-04-11 01:13:56 +0530

    SharpenerProjects

---
+++ app.js
@@ -0,0 +1,28 @@
+// console.log(document.domain);
+// console.log(document.title);
+// // document.title = 123
+// console.log(document.head);
+// console.log(document.URL);
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+//getElemntsById//
+// console.log(document.getElementById('header-title'));
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent='hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// console.log(headerTitle.textContent);
+// headerTitle.innerHTML=<h3>Hello</h3>
+// headerTitle.style.borderBottom ='solid 3px #000'
+// header.style.borderBottom ='solid 3px #000'
+// let main = document.getElementById('main');
+// main.style.fontWeight = 'bold'
+// main.style.color = 'green';
